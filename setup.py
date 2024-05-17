88ed5f6b86d933a82062e901449677f6cb705024
seojinyoon <dbstjwls1234@hanmail.net>
2024-05-17 17:38:45 +0900

    0.0.1.dev5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = "boostrsa",
-    version = "0.0.1dev4",
+    version = "0.0.1dev5",
     author = "seojin",
     author_email = "pures1@hanyang.ac.kr",
     description = "This is toolbox for boosting calculation speed using GPU",
